26eec78ab7124438aabfcd5b10015344d6cdf04d
Arpa-exe <oli.laroch@gmail.com>
2025-03-19 11:30:09 -0400

    spells fx

--- js/app.js
+++ js/app.js
@@ -9,3 +9,10 @@
   document.getElementById('toggleButtonFree').addEventListener('click', function() {
     this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
   });
+  document.getElementById('toggleButtonCantrips').addEventListener('click', function() {
+    this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
+  });
+
+  document.getElementById('toggleButtonSpells').addEventListener('click', function() {
+    this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
+  });
