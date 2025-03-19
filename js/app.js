17bc95afeb13d382c24d73025f69cdc8f833eb02
Arpa-exe <oli.laroch@gmail.com>
2025-03-19 10:10:50 -0400

    action and bonus ali

---
+++ js/app.js
@@ -0,0 +1,7 @@
+  document.getElementById('toggleButton').addEventListener('click', function() {
+  this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
+});
+
+  document.getElementById('toggleButtonBonus').addEventListener('click', function() {
+    this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
+  });
