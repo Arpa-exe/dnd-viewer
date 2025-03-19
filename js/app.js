f2bb1bdaaa32cec88b92cbf03c6d83764cf037be
Arpa-exe <oli.laroch@gmail.com>
2025-03-19 10:18:06 -0400

    ali complete

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@
   document.getElementById('toggleButtonBonus').addEventListener('click', function() {
     this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
   });
+
+  document.getElementById('toggleButtonFree').addEventListener('click', function() {
+    this.textContent = this.textContent === "Show More" ? "Show Less" : "Show More";
+  });
